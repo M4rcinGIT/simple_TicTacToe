932667d3380b2646317e4231730aac71da0b5499
Marcin <133963751+M4rcinGIT@users.noreply.github.com>
2023-05-19 13:49:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 class Board():
+        #constructor
     def __init__(self):
-        self.tablica = [[' ', ' ', ' '],
+        self.board = [[' ', ' ', ' '],
                         [' ', ' ', ' '],
                         [' ', ' ', ' ']]
-
+        #printing current state of the board 
     def display(self):
         print("---------")
-        for wiersz in self.tablica:
+        for I in self.board:
             print("|", end=" ")
-            for element in wiersz:
+            for element in I:
                 print(element, end=" ")
             print("|")
         print("---------")
-
+        #incializing starting board(for testing only)
     def get_starting_board(self):
         u = list(input("> "))
         for i in range(3):
             for j in range(3):
-                self.tablica[i][j] = u.pop(0)
-        return  self.tablica
-
+                self.board[i][j] = u.pop(0)
+        return  self.board
+        #input from user and checking if it is valid, as argument method takes 'O' or 'X'
     def move(self, x_or_o):
         while True:
             move = input('> ')
@@ -34,48 +35,52 @@ class Board():
                 print('Coordinates should be from 1 to 3!')
                 continue
 
-            if self.tablica[i-1][j-1] != " ":
+            if self.board[i-1][j-1] != " ":
                 print("This cell is occupied! Choose another one!")
                 continue
             break
-        self.tablica[i-1][j-1] = x_or_o
-
+        self.board[i-1][j-1] = x_or_o
+            #method checks if game is over
     def is_over(self):
-        for i in range(len(self.tablica)):
-            if self.tablica[i][0] != ' ' and all(self.tablica[i][0] == self.tablica[i][j] for j in range(1, len(self.tablica))):
-                print(f'{self.tablica[i][0]} wins!')
+            # checking for win in level
+        for i in range(len(self.board)): 
+            if self.board[i][0] != ' ' and all(self.board[i][0] == self.board[i][j] for j in range(1, len(self.board))):
+                print(f'{self.board[i][0]} wins!')
                 return True
-
-        for i in range(len(self.tablica)):
-            if self.tablica[0][i] != ' ' and  all(self.tablica[0][i] == self.tablica[j][i] for j in range(1, len(self.tablica))):
-                print(f'{self.tablica[0][i]} wins!')
+            # checking for win vertically
+        for i in range(len(self.board)):
+            if self.board[0][i] != ' ' and  all(self.board[0][i] == self.board[j][i] for j in range(1, len(self.board))):
+                print(f'{self.board[0][i]} wins!')
                 return True
-
-        if self.tablica[0][0] != ' ' and all(self.tablica[0][0] == self.tablica[i][i] for i in range(1, len(self.tablica))):
-            print(f'{self.tablica[0][0]} wins!')
+            #checking for win in first diagonal
+        if self.board[0][0] != ' ' and all(self.board[0][0] == self.board[i][i] for i in range(1, len(self.board))):
+            print(f'{self.board[0][0]} wins!')
             return  True
-
-        if self.tablica[0][len(self.tablica) - 1] != ' ' and all(self.tablica[0][len(self.tablica) - 1] == self.tablica[i][len(self.tablica) - 1 - i] for i in range(1, len(self.tablica))):
-            print(f'{self.tablica[0][len(self.tablica) - 1]} wins!')
+            #checking for win in second diagonal
+        if self.board[0][len(self.board) - 1] != ' ' and all(self.board[0][len(self.board) - 1] == self.board[i][len(self.board) - 1 - i] for i in range(1, len(self.board))):
+            print(f'{self.board[0][len(self.board) - 1]} wins!')
             return True
-
-        for i in range(len(self.tablica)):
-            for j in range(len(self.tablica)):
-                if self.tablica[i][j] == ' ':
+            #checking if board has any free pleaces
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                if self.board[i][j] == ' ':
                     return False
         print("It's a draw")
         return True
+def main():
+    play_board = Board()
+    play_board.display()
+    while True:
+        play_board.move('X')
+        play_board.display()
+        if play_board.is_over() == True:
+            break
+        play_board.move('O')
+        play_board.display()
+        if play_board.is_over() == True:
+            break
 
-board = Board()
-board.display()
-while True:
-    board.move('X')
-    board.display()
-    if board.is_over() == True:
-        break
-    board.move('O')
-    board.display()
-    if board.is_over() == True:
-        break
+if __name__ == '__main__':
+    main()
 
 
